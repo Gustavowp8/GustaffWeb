a23cf488997b0610818874002deffa16559e2b56
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2024-12-27 17:33:20 -0300

    Adicionar arquivos de projeto.

---
+++ GustaffWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GustaffWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
