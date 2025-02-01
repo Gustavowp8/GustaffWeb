720345a484a5769172a18a2a91090a320ac4d9e8
Gustavo73 <gustavoferreira73@live.com>
2025-02-01 19:17:59 -0300

    Finalizando a tela de inicio

--- GustaffWeb/wwwroot/js/site.js
+++ GustaffWeb/wwwroot/js/site.js
@@ -6,3 +6,7 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+function chamaWhast() {
+    location = 'https://wa.me/556135752752?text=Ol%C3%A1%2C+preciso+esclarecer+uma+duvida'
+}
